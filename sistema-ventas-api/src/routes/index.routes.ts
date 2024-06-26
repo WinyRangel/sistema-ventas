8acb819ee61d00d2f9ca4c062ff971d031c3d8f5
Winy <danielamanzanorangel@email.com>
2024-06-25 20:30:35 -0600

    Se configuró swagger

--- sistema-ventas-api/src/routes/index.routes.ts
+++ sistema-ventas-api/src/routes/index.routes.ts
@@ -9,12 +9,67 @@ class IndexRoutes{
         this.router = Router();
         this.config();
     }
+    /**
+     * 
+     */
 
 
     config(): void {
+        /**
+         * @swagger
+         * /api:
+         *  get:
+         *      tags: ["Index"]
+         *      summary: Default Index
+         *      description: Ruta por defecto de la API
+         *      produces:
+         *          - application/json
+         *      responses:
+         *          200:
+         *              description:
+         */
         this.router.get('/', indexController.index);
+        /**
+         * @swagger
+         * /api:
+         *  post:
+         *      tags: ["Index"]
+         *      summary: Default Index (Insert)
+         *      description: Ruta por defecto de la API (Insert)
+         *      produces:
+         *          - application/json
+         *      responses:
+         *          200:
+         *              description:
+         */
         this.router.post('/', indexController.insert);
+        /**
+         * @swagger
+         * /api:
+         *  put:
+         *      tags: ["Index"]
+         *      summary: Default Index (Put)
+         *      description: Ruta por defecto de la API (Put)
+         *      produces:
+         *          - application/json
+         *      responses:
+         *          200:
+         *              description:
+         */
         this.router.put('/', indexController.update);
+        /**
+         * @swagger
+         * /api:
+         *  delete:
+         *      tags: ["Index"]
+         *      summary: Default Index (Delete)
+         *      description: Ruta por defecto de la API (Delete)
+         *      produces:
+         *          - application/json
+         *      responses:
+         *          200:
+         *              description:
+         */
         this.router.delete('/', indexController.delete);
      }
 }
